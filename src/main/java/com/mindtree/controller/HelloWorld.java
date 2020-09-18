6929cc5ca31cdf7a98d35e2c6d5cf28778a9db9c
shivkailash123 <shivkailash12345@gmail.com>
2020-09-18 15:23:29 +0530

    first commit

---
+++ src/main/java/com/mindtree/controller/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.mindtree.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorld {
+
+	
+	@GetMapping
+	public String hello() {
+		return "<h2>Welcome to new World</h2>";
+	}
+}
